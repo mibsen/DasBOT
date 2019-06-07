fa88acebc1569a262ac0e6f1591790e44c9af9c8
mibsen <mibsen>
2019-06-07 13:52:57 +0200

    Added Multi pickup for easy balls

--- src/bot/states/CollectBalls.java
+++ src/bot/states/CollectBalls.java
@@ -2,6 +2,9 @@ package bot.states;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
@@ -34,13 +37,13 @@ import services.WallService;
 public class CollectBalls extends State {
 
 	private CarService carService;
-	int timeout = 30;
 	private BallService ballService;
 	private WallService wallService;
 	private Map map;
-	private Ball activeBall = null;
-	
-	
+	private boolean waitForNextFrame = false;
+	private ArrayList<Ball> activeBalls = new ArrayList<Ball>();
+	private long timeout = 120;
+
 	public CollectBalls(CarService carService, BallService ballService, WallService wallService) {
 
 		this.carService = carService;
@@ -51,39 +54,38 @@ public class CollectBalls extends State {
 	@Override
 	public State process(Mat frame) {
 
-		
+		final Mat f = frame.clone();
+
+		// We need car Position and Wall
+
 		Wall wall = null;
 		Car car = null;
 		List<Ball> balls = null;
 
-		
-	 wall = wallService.getWall(frame);
-			
+		wall = wallService.getWall(frame);
 
-	    car = carService.getCar(frame);
+		car = carService.getCar(frame);
 
-				balls = ballService.getBalls(frame);
-	
-				if(wall == null || car == null) {
-					return this;
-				}
+		balls = ballService.getBalls(frame);
 
+		if (wall == null || car == null) {
+			return this;
+		}
 
-		map = new Map(car, frame);
+		map = new Map(car, f);
 		map.addBalls(balls);
 		map.addWall(wall);
 
 		map.corrected();
 
 		map.drawCar(new Scalar(0, 250, 250), 1);
-		map.drawBalls(new Scalar(0, 250, 250), 1);
 		map.drawWall(new Scalar(250, 250, 250), (int) (car.width * 2));
-		
+		map.drawBalls(new Scalar(0, 250, 250), 1);
+
 		Mat m = map.getFrame();
 
 		if (running == null) {
-			
-			
+
 			// Locate A Ball
 
 			if (map.balls.size() == 0) {
@@ -91,64 +93,102 @@ public class CollectBalls extends State {
 				return this;
 			}
 
-			Ball ball = null;
+			// Ball ball = null;
 
 			// Close - Not to close
 			// We do not want balls to close!
 			double distance = Double.MAX_VALUE;
 			double minDistance = car.width * 2;
 
+			// Remove close balls and balls close to border
 			for (Ball b : map.balls) {
 
 				double d = Math.sqrt(Math.pow(b.point.x, 2) + Math.pow(b.point.y, 2));
 
-				if (d <= distance && d >= minDistance) {
-
-					// Not against Wall
-					if (new Scalar(m.get((int) (b.point.y + map.center.y), (int) (b.point.x + map.center.x))).equals(new Scalar(250, 250, 250))) {
-						System.out.println("THIS BALL IS TO CLOSE TO THE BORDER!");
-					} else {
-						distance = d;
-						ball = b;
-					}
+				if (d <= minDistance) {
+					map.balls.remove(b);
+				} else if (new Scalar(m.get((int) (b.point.y), (int) (b.point.x))).equals(new Scalar(250, 250, 250))) {
+					map.balls.remove(b);
 				}
-
 			}
 
-			if (ball == null) {
-				System.out.println("COULD NOT LOCATE BALL");
+			if (map.balls.size() == 0) {
+
+				System.out.println("THERE IS NO BALLS TO COLLECT!!!");
 				return this;
 			}
-			
+
 			running = LocalTime.now();
 
+			// Sort Balls by distance to car
+
+			Collections.sort(map.balls, new Comparator<Ball>() {
+
+				@Override
+				public int compare(Ball o1, Ball o2) {
+
+					double d1 = Math.sqrt(Math.pow(o1.point.x, 2) + Math.pow(o1.point.y, 2));
+					double d2 = Math.sqrt(Math.pow(o2.point.x, 2) + Math.pow(o2.point.y, 2));
+
+					if (d1 == d2) {
+						return 0;
+					}
+					return d1 > d2 ? 1 : -1;
+				}
+			});
+
+			/*
+			 * for (Ball b : map.balls) {
+			 * 
+			 * double d = Math.sqrt(Math.pow(b.point.x, 2) + Math.pow(b.point.y, 2));
+			 * 
+			 * if (d <= distance && d >= minDistance) {
+			 * 
+			 * // Not against Wall if (new Scalar(m.get((int) (b.point.y), (int)
+			 * (b.point.x))).equals(new Scalar(250, 250, 250))) {
+			 * System.out.println("THIS BALL IS TO CLOSE TO THE BORDER!"); } else { distance
+			 * = d; ball = b; } }
+			 * 
+			 * }
+			 * 
+			 * if (ball == null) { System.out.println("COULD NOT LOCATE BALL"); return this;
+			 * }
+			 * 
+			 */
+			activeBalls = new ArrayList<Ball>();
+
+			for (Ball b : map.balls) {
+				activeBalls.add(new Ball(map.getOriginalPoint(b.point), b.area));
+			}
+
 			// Save the located active ball
-			activeBall = new Ball(map.getOriginalPoint(ball.point), ball.area);
+			// activeBalls = new Ball(map.getOriginalPoint(ball.point), ball.area);
 
-			
-			System.out.println("Driving to: " + ball.point.toString());
+			// Drive through while motor is running
 
-			
+			System.out.println("Driving to first ball: " + map.balls.get(0).point.toString());
+
 			double ratio = car.widthInCM / car.width;
-			float nx = (float) (ball.point.x*ratio);
-			float ny = (float) (-1 * ball.point.y*ratio);
-			System.out.println(car.width);
-			System.out.println("Driving to: " +  nx + " : " + ny);
 
-			
 			ActionList list = new ActionList();
 			list.add(new StartCollectionAction());
-			list.add(new WayPointAction(nx, ny, 0.8F));
+
+			for (Ball ball : map.balls) {
+				float nx = (float) (ball.point.x * ratio);
+				float ny = (float) (-1 * ball.point.y * ratio);
+				System.out.println("Driving to: " + nx + " : " + ny);
+				list.add(new WayPointAction(nx, ny, 0.80F));
+			}
+
 			list.add(new WaitAction(3000));
 			list.add(new StopCollectionAction());
 			list.add(new TestAction("-DONE-"));
-			
-			if(!Bot.test)
-			Connection.SendActions(list);
+
+			if (!Bot.test)
+				Connection.SendActions(list);
 
 		}
-		
-		
+
 		// Verify the Timeout
 		if (Duration.between(running, LocalTime.now()).getSeconds() > timeout) {
 
@@ -162,8 +202,25 @@ public class CollectBalls extends State {
 			return this;
 		}
 
-		Imgproc.line(m, map.center, map.correctPoint(activeBall.point), new Scalar(88, 214, 141));
-		Imgproc.line(frame, car.center, activeBall.point, new Scalar(88, 214, 141));
+		// Draw robot frame
+		Ball first = activeBalls.get(0);
+		Imgproc.line(m, map.center, map.correctPoint(first.point), new Scalar(88, 214, 141));
+
+		for (int i = 1; i < activeBalls.size(); i++) {
+			Ball sec = activeBalls.get(i);
+			Imgproc.line(m, map.correctPoint(first.point), map.correctPoint(sec.point), new Scalar(88, 214, 141));
+			first = sec;
+		}
+
+		// Draw original frame
+		first = activeBalls.get(0);
+		Imgproc.line(frame, car.center, first.point, new Scalar(88, 214, 141));
+
+		for (int i = 1; i < activeBalls.size(); i++) {
+			Ball sec = activeBalls.get(i);
+			Imgproc.line(frame, first.point, sec.point, new Scalar(88, 214, 141));
+			first = sec;
+		}
 
 		return this;
 	}
@@ -177,13 +234,13 @@ public class CollectBalls extends State {
 		return map.getFrame();
 	}
 
-	
 	public void handle(String message) {
 
 		// We are done and we are ready for new work!
 		if (message.equals(Messages.DONE)) {
-			activeBall = null;
+			activeBalls = new ArrayList<Ball>();
 			running = null;
 		}
 	}
+
 }
