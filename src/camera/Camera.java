4b748a77080041593777dea1144f6265ba14634d
LordHjorth <Hjorthen96@hotmail.com>
2019-06-12 13:11:52 +0200

    HEJ MARTIN

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -6,6 +6,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.calib3d.Calib3d;
 
 public class Camera implements CameraInterface {
 
@@ -42,25 +43,25 @@ public class Camera implements CameraInterface {
 			return null;
 		}
 
-		//K, D, np.eye(3), K, DIM, 
+	//K, D, np.eye(3), K, DIM, 
 		
 		Mat map1 = new Mat();
 		Mat map2 = new Mat();
 		
 	
-		  int[][] eyeArray = new int[][]{{1,0,0},{0,1,0},{0,0,1}};
+			  int[][] eyeArray = new int[][]{{1,0,0},{0,1,0},{0,0,1}};
 		  Mat eye = new Mat(3,3,CvType.CV_8UC1);
 		    for(int row=0;row<3;row++){
-		        for(int col=0;col<4;col++)
+		        for(int col=0;col<3;col++)
 		            eye.put(row, col, eyeArray[row][col]);
 		    }
 		
-		
+			
 		// D = distCoeffs    
 		  double[][] dArray = new double[][]{{0.4108889201891428},{-0.0029018582999649468},{-0.019218620930577385},{-0.2094587331452949}};
 		  Mat d = new Mat(4,1,CvType.CV_8UC1);
-		    for(int row=0;row<3;row++){
-		        for(int col=0;col<4;col++)
+		    for(int row=0;row<4;row++){
+		        for(int col=0;col<1;col++)
 		            d.put(row, col, dArray[row][col]);
 		    }
 	
@@ -69,17 +70,17 @@ public class Camera implements CameraInterface {
 			  double[][] kArray = new double[][]{{502.26087138645096, 0.0, 358.7882796447911},{0.0, 502.1027412727765, 244.26495016375978},{0.0, 0.0, 1.0}};
 			  Mat k = new Mat(3,3,CvType.CV_8UC1);
 			    for(int row=0;row<3;row++){
-			        for(int col=0;col<4;col++)
-			            d.put(row, col, dArray[row][col]);
+			        for(int col=0;col<3;col++)
+			            k.put(row, col, kArray[row][col]);
 			    }
-		   
-		Imgproc.initUndistortRectifyMap(k, d, eye, k, frame.size(), CvType.CV_16SC2, map1, map2);		
+				
+		Calib3d.initUndistortRectifyMap(k, d, eye, k, frame.size(), CvType.CV_16SC2, map1, map2);		
 		Imgproc.remap(frame, frame, map1, map2, Imgproc.INTER_LINEAR, Core.BORDER_CONSTANT);
 
 		
-		Size sz = new Size(frame.width()/2,frame.height()/2);
-		Imgproc.resize( frame, frame, sz );
-		 
+		Size sz = new Size(frame.width() / 2, frame.height() / 2);
+		Imgproc.resize(frame, frame, sz);
+
 		return frame;
 
 	}
--- src/camera/CameraFake.java
+++ src/camera/CameraFake.java
@@ -2,6 +2,7 @@ package camera;
 
 import java.io.File;
 
+import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -67,8 +68,6 @@ public class CameraFake  implements CameraInterface{
 		            d.put(row, col, dArray[row][col]);
 		    }
 	
-		    System.out.println("TYHIS");
-
 			// K = cameraMatric
 		    // ([[502.26087138645096, 0.0, 358.7882796447911], [0.0, 502.1027412727765, 244.26495016375978], [0.0, 0.0, 1.0]])
 			  double[][] kArray = new double[][]{{502.26087138645096, 0.0, 358.7882796447911},{0.0, 502.1027412727765, 244.26495016375978},{0.0, 0.0, 1.0}};
@@ -77,9 +76,8 @@ public class CameraFake  implements CameraInterface{
 			        for(int col=0;col<3;col++)
 			            k.put(row, col, kArray[row][col]);
 			    }
-			    System.out.println("TYHIS!!");
 				
-		Imgproc.initUndistortRectifyMap(k, d, eye, k, frame.size(), CvType.CV_16SC2, map1, map2);		
+		Calib3d.initUndistortRectifyMap(k, d, eye, k, frame.size(), CvType.CV_16SC2, map1, map2);		
 		Imgproc.remap(frame, frame, map1, map2, Imgproc.INTER_LINEAR, Core.BORDER_CONSTANT);
 
 		/*
